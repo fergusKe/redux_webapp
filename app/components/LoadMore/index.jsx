9e69741b41f585c56fd24667add1164d67cf3503
fergus <fergus.jingtai@gmail.com>
2017-07-03 00:27:57 +0800

    7-21 加载更多-下拉效果

--- app/components/LoadMore/index.jsx
+++ app/components/LoadMore/index.jsx
@@ -23,6 +23,28 @@ class LoadMore extends React.Component {
 		// 执行传输过来的 loadMoreFn
 		this.props.loadMoreFn()
 	}
+	componentDidMount() {
+		const loadMoreFn = this.props.loadMoreFn
+		const wrapper = this.refs.wrapper
+		let timeoutId
+		function callback() {
+			const top = wrapper.getBoundingClientRect().top
+			const windowHeight = window.screen.height
+			if (top && top < windowHeight) {
+				loadMoreFn()
+			}
+		}
+
+		window.addEventListener('scroll', function() {
+			if (this.props.isLoadingMore) {
+				return
+			}
+			if (timeoutId) {
+				clearTimeout(timeoutId)
+			}
+			timeoutId = setTimeout(callback, 50) // 確保在50毫秒裡只會執行一次
+		}.bind(this), false)
+	}
 }
 
 export default LoadMore
