162c55e1edf8956a2382c03e1881ad57224ea8bf
fergus <fergus.jingtai@gmail.com>
2017-07-02 08:49:08 +0800

    7-7 HomeHeader组件（2）

--- app/components/HomeHeader/index.jsx
+++ app/components/HomeHeader/index.jsx
@@ -10,17 +10,20 @@ class HomeHeader extends React.Component {
   }
   render() {
     return (
-			<div className="clear-fix">
-				<div className="float-left">
-					深圳
+			<div id="home-header" className="clear-fix">
+				<div className="home-header-left float-left">
+					<span>深圳</span>
+					&nbsp;
 					<i className="icon-angle-down"></i>
 				</div>
-				<div className="float-right">
+				<div className="home-header-right float-right">
 					<i className="icon-user"></i>
 				</div>
-				<div>
-					<i className="icon-search"></i>
-					<input />
+				<div className="home-header-middle">
+					<div className="search-container">
+						<i className="icon-search"></i>
+						<input type="text" placeholder="请输入关键字" />
+					</div>
 				</div>
 			</div>
     )
