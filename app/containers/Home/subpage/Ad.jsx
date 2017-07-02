04eea291dc963ee51df69b8b8ea79dc3220aa921
fergus <fergus.jingtai@gmail.com>
2017-07-02 11:20:00 +0800

    7-13 超值特惠-获取数据

---
+++ app/containers/Home/subpage/Ad.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import { getAdData } from '../../../fetch/home/home.js'
+
+class Ad extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+		this.state = {
+			data: []
+		}
+  }
+  render() {
+    return (
+      <h1>{this.state.data.length}</h1>
+    )
+  }
+	componentDidMount() {
+		const result = getAdData()
+		result.then((res) => {
+			return res.json()
+		}).then((json) => {
+			const data = json
+			if (data.length) {
+				this.setState({
+					data
+				})
+			}
+		})
+	}
+}
+
+export default Ad
