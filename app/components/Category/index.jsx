5e6ca9521ee2855f3e995bb3c0bc235c8c401835
fergus <fergus.jingtai@gmail.com>
2017-07-02 09:53:54 +0800

    7-10 获取轮播图 index

--- app/components/Category/index.jsx
+++ app/components/Category/index.jsx
@@ -6,14 +6,31 @@ class Category extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    this.state = {
+      index: 0
+    }
   }
   render() {
+    let opt = {
+      auto: 2000,
+      callback: function(index) {
+        this.setState({
+          index
+        })
+      }.bind(this)
+    }
+
     return (
-      <ReactSwipe className="carousel" swipeOptions={{continuous: false}}>
-        <div>PANE 1</div>
-        <div>PANE 2</div>
-        <div>PANE 3</div>
-      </ReactSwipe>
+      <div>
+        <ReactSwipe className="carousel" swipeOptions={opt}>
+          <div>PANE 1</div>
+          <div>PANE 2</div>
+          <div>PANE 3</div>
+        </ReactSwipe>
+        <div>
+          {this.state.index}
+        </div>
+      </div>
     )
   }
 }
